e7623964f426a49ef7b464498e3c00bfbd02b256
Dao An Phuoc <anphuocdao2410@gmail.com>
2025-05-03 18:09:33 +0700

    feat: Add EvolutionTab component for displaying Pokémon evolution data with loading and no evolution states.

---
+++ pokedexreactasp.client/src/pages/Detail/tabs/EvolutionTab.style.ts
@@ -0,0 +1,20 @@
+import styled from "@emotion/styled";
+
+export const EvolutionContainer = styled.div`
+  padding: 16px 0;
+`;
+
+export const LoadingContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 200px;
+  width: 100%;
+`;
+
+export const NoEvolutionMessage = styled.div`
+  text-align: center;
+  padding: 32px 0;
+  color: #6B7280;
+  font-style: italic;
+`;
