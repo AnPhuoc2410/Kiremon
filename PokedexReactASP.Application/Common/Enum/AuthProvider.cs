0db028ec5ec2317f5fe9b92b76a329e8a2bb84bc
Vũ Hoàng Hiếu Ngân <vuhoanghieungan564@gmail.com>
2025-12-11 22:36:46 +0700

    feat: add AuthProvider enum for social authentication options

---
+++ PokedexReactASP.Application/Common/Enum/AuthProvider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokedexReactASP.Application.Common.Enum
+{
+    public enum AuthProvider
+    {
+        Google,
+        Facebook,
+        GitHub
+    }
+}
