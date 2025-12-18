2a03dfd138a1197849e4de16e6e59d54fbe59c52
Dao An Phuoc <anphuocdao2410@gmail.com>
2025-12-18 15:39:44 +0700

    feat: add IPokemonCacheService and IPokemonEnricher interfaces for caching and enriching Pokémon data

---
+++ PokedexReactASP.Application/Interfaces/IPokemonCacheService.cs
@@ -0,0 +1,16 @@
+﻿using PokedexReactASP.Application.Services;
+
+namespace PokedexReactASP.Application.Interfaces
+{
+    /// <summary>
+    /// Caches PokeAPI data to avoid repeated API calls.
+    /// Pokemon data is static (Pikachu's stats never change), so we cache aggressively.
+    /// </summary>
+    public interface IPokemonCacheService
+    {
+        Task<PokeApiPokemon?> GetPokemonAsync(int pokemonApiId);
+        Task<PokeApiPokemon?> GetPokemonAsync(string name);
+        Task<Dictionary<int, PokeApiPokemon>> GetPokemonBatchAsync(IEnumerable<int> pokemonApiIds);
+        void InvalidateCache(int pokemonApiId);
+    }
+}
---
+++ PokedexReactASP.Application/Interfaces/IPokemonEnricher.cs
@@ -0,0 +1,17 @@
+﻿using PokedexReactASP.Application.DTOs.Pokemon;
+using PokedexReactASP.Application.Services;
+using PokedexReactASP.Domain.Entities;
+
+namespace PokedexReactASP.Application.Interfaces
+{
+    /// <summary>
+    /// Responsible for enriching UserPokemon entities with PokeAPI data.
+    /// Separated from UserService to follow Single Responsibility Principle.
+    /// </summary>
+    public interface IPokemonEnricher
+    {
+        Task<UserPokemonDto> EnrichAsync(UserPokemon userPokemon);
+        Task<IReadOnlyList<UserPokemonDto>> EnrichBatchAsync(IEnumerable<UserPokemon> userPokemonList);
+        UserPokemonDto EnrichWithCachedData(UserPokemon userPokemon, PokeApiPokemon? pokeApiData);
+    }
+}
