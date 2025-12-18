8bb7fa55ac3c4f8b7101282af56ed36564745a78
Dao An Phuoc <anphuocdao2410@gmail.com>
2025-12-18 23:22:13 +0700

    fix: update using directives in IPokemonFactoryService and IShinyRollerService to correct namespaces; add CatchCalculationContext for catch calculation logic

--- PokedexReactASP.Application/Interfaces/IGameMechanics/IShinyRollerService.cs
+++ PokedexReactASP.Application/Interfaces/IGameMechanics/IShinyRollerService.cs
@@ -1,4 +1,4 @@
-﻿using PokedexReactASP.Application.Services.GameMechanics;
+﻿using PokedexReactASP.Application.Models.GameMechanics;
 
 namespace PokedexReactASP.Application.Interfaces.IGameMechanics
 {
