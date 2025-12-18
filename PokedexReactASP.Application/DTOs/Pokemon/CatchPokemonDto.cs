209b7a21d1f99f78c81a465817ca935833c29bd2
Dao An Phuoc <anphuocdao2410@gmail.com>
2025-12-18 23:22:38 +0700

    feat: add CatchAttemptDto and CatchAttemptResultDto for catch attempt requests and responses; update UserPokemonDto with additional fields for server-determined characteristics

--- PokedexReactASP.Application/DTOs/Pokemon/CatchPokemonDto.cs
+++ PokedexReactASP.Application/DTOs/Pokemon/CatchPokemonDto.cs
@@ -2,9 +2,6 @@ using System.ComponentModel.DataAnnotations;
 
 namespace PokedexReactASP.Application.DTOs.Pokemon
 {
-    /// <summary>
-    /// DTO for catching a new Pokemon from PokeAPI
-    /// </summary>
     public class CatchPokemonDto
     {
         [Required]
@@ -12,31 +9,9 @@ namespace PokedexReactASP.Application.DTOs.Pokemon
 
         [StringLength(50)]
         public string? Nickname { get; set; }
-        
+
         [StringLength(100)]
         public string? CaughtLocation { get; set; }
-        
-        [Range(1, 100)]
-        public int CaughtLevel { get; set; } = 5;
-        
-        public bool IsShiny { get; set; } = false;
-        
-        [Range(0, 31)]
-        public int? IvHp { get; set; }
-        
-        [Range(0, 31)]
-        public int? IvAttack { get; set; }
-        
-        [Range(0, 31)]
-        public int? IvDefense { get; set; }
-        
-        [Range(0, 31)]
-        public int? IvSpecialAttack { get; set; }
-        
-        [Range(0, 31)]
-        public int? IvSpecialDefense { get; set; }
-        
-        [Range(0, 31)]
-        public int? IvSpeed { get; set; }
+
     }
 }
