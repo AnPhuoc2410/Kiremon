0e9ae3119922427e7237c94aa3ecd0196fd8f88d
Dao An Phuoc <anphuocdao2410@gmail.com>
2026-01-08 13:57:18 +0700

    test husky

---
+++ pokedexreactasp.client/HUSKY_TEST.ts
@@ -0,0 +1,8 @@
+// Test file for Husky pre-commit hook
+// This code is intentionally unformatted to test auto-formatting
+
+const test={a:1,b:2,c:3}
+const array=[1,2,3,4,5]
+function hello(name,age){return `Hello ${name}, ${age}`}
+
+export default test
